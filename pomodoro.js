5eef4c685453784e256a4af6bfd8a5075e6a2d78
Mark Bungeroth <mbungeroth@gmail.com>
2018-05-23 21:41:19 -0400

    Add start button functionality. Get reset to stop time. Still need to get it to reset.

--- pomodoro.js
+++ pomodoro.js
@@ -1,1 +1,47 @@
-let timer = document.getElementById('timeLeft').textContent;
+const timer = document.getElementById('timeLeft');
+const start = document.getElementById('sButton');
+const reset = document.getElementById('reset');
+
+let cancelled = false;
+let timeRemaining = 1500;
+
+start.addEventListener('click', startTimer);
+reset.addEventListener('click', resetTime);
+
+function startTimer() {
+  cancelled = false;
+  decreaseTimer();
+}
+
+function displayTime(timeRemaining) {
+  let minutes = Math.floor(timeRemaining / 60);
+  let seconds = timeRemaining % 60;
+
+  if (minutes < 10) {
+    minutes = 0 + String(minutes);
+  }
+  if (seconds < 10) {
+    seconds = 0 + String(seconds);
+  }
+
+  return `${minutes}:${seconds}`;
+}
+
+function decreaseTimer() {
+  let decreaser = setTimeout(function decreaseTime() {
+    if (cancelled === false) {
+      timeRemaining -= 1;
+      timer.textContent = displayTime(timeRemaining)
+      decreaser = setTimeout(decreaseTime, 1000);
+    } else {
+      timeRemaining = 1500;
+      displayTime(timeRemaining);
+      return;
+    }
+  }, 1000);
+}
+
+
+function resetTime() {
+  cancelled = true;
+}
